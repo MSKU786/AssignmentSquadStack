f2ebe97f907be7e3587663f2d28360f371f7352d
MANISH SINGH <manishsingh332000@gmail.com>
2021-07-08 06:09:40 +0530

    Topbar done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Topbar from "./Components/Topbar/Topbar"
 
 function App() {
   return (
     <div className="App">
-      
+        <Topbar defaultValue = {2}/>
     </div>
   );
 }
--- src/Components/Topbar/topbar.jsx
+++ src/Components/Topbar/topbar.jsx
@@ -1,52 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
-function topbar(props) {
-    const [range, setRange] = useState(2);
+function Topbar({defaultValue}) {
+ 
+    const [range, setRange] = useState(defaultValue);
+    useEffect(() => {
+        setRange(JSON.parse(window.localStorage.getItem('range')));
+      }, []);
+    
+      useEffect(() => {
+        window.localStorage.setItem('range', range);
+      }, [range]);
     return (
         <div>
-            <div className="first" onClick = {()=> setRange(0)}>
+            <div className="first" onClick = {()=> setRange(1)}>
             <span>$100K-$200K</span>
             {
-                range ==0 && 
+                range ===1 && 
                 <div>
                     <h2>first</h2>
                 </div>
             }
             </div>
-            <div className="second" onClick = {()=> setRange(1)}>
+            <div className="second" onClick = {()=> setRange(2)}>
             <span>$200K-$300K</span>
             {
-                    range == 1 && 
+                    range === 2 && 
                     <div>
                         <h2>Second</h2>
                     </div>
             }
             </div>
-            <div className="third" onClick = {()=> setRange(2)}>
+            <div className="third" onClick = {()=> setRange(3)}>
             <span>$300K-$400K</span>
             {
-                    range == 2 && 
+                    range === 3 && 
                     <div>
                         <h2>Third</h2>
                     </div>
             }
 
             </div>
-            <div className="fourth" onClick = {()=> setRange(3)}>
+            <div className="fourth" onClick = {()=> setRange(4)}>
             <span>$400K-$500K</span>
             {
-                    range == 3 && 
+                    range === 4 && 
                     <div>
                         <h2>Fourth</h2>
                     </div>
             }
 
             </div>
-            <div className="fifth" onClick = {()=> setRange(4)}>
+            <div className="fifth" onClick = {()=> setRange(5)}>
             <span>$500K-$600K</span>
             {
-                    range == 4 && 
+                    range === 5 && 
                     <div>
                         <h2>five</h2>
                     </div>
@@ -56,4 +64,4 @@ function topbar(props) {
     );
 }
 
-export default topbar;
+export default Topbar;
